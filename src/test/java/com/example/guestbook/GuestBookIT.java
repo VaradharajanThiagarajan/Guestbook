19494df1d1b46d35533eb8ec1c3c7af4b703cf97
varadharajanthiagarajan <varadha.001@gmail.com>
2021-04-06 19:32:05 -0400

    Commit for green - added rest documentation

--- src/test/java/com/example/guestbook/GuestBookIT.java
+++ src/test/java/com/example/guestbook/GuestBookIT.java
@@ -3,19 +3,24 @@ package com.example.guestbook;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDocs;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.web.servlet.MockMvc;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+
+import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
+import static org.springframework.restdocs.payload.PayloadDocumentation.fieldWithPath;
+import static org.springframework.restdocs.payload.PayloadDocumentation.responseFields;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
 @AutoConfigureMockMvc
-
+@AutoConfigureRestDocs
 public class GuestBookIT {
 
     @Autowired
@@ -40,11 +45,21 @@ public class GuestBookIT {
         mockMvc.perform(post("/Guestentries/Entries")
                 .content(objectMapper.writeValueAsString(visitor1))
                 .contentType(MediaType.APPLICATION_JSON)
-        ).andExpect(status().isCreated());
+        ).andExpect(status().isCreated())
+                .andDo(document("AddGuestEntries"));
+
 
         mockMvc.perform(get("/Guestentries/Entries")
         ).andExpect(status().isOk())
                 .andExpect(jsonPath("length()").value(1))
-                .andExpect(jsonPath("[0].name").value("Varadha"));
+                .andExpect(jsonPath("[0].name").value("Varadha"))
+                .andExpect(jsonPath("[0].comments").value("The Museum of fine arts is really good"))
+                .andDo(document("GetEntries", responseFields(
+                        fieldWithPath("[0].name").description("name of the Guest"),
+                        fieldWithPath("[0].comments").description("Comments of guest")
+                )));
+
+
+
     }
 }
